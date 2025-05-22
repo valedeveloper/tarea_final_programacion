6088bdb34a3aabc8bbd42750b102382ae5fd462c
valedeveloper <vjimenezbedoya@gmail.com>
2025-05-22 00:07:15 -0500

    Agregada Interfaz.py y cambios en logica.py

---
+++ Interfaz.py
@@ -0,0 +1,126 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import font
+import random
+import math
+import csv
+import os 
+from logica import Recetario
+
+base = tk.Tk()
+base.resizable(False, False)
+
+base.title("Gestor de Recetas")
+
+text_area = tk.Text(base, width=80, height=20)
+
+recetario = Recetario()
+
+# Funciones conectadas a botones
+
+def agregar_receta():
+    nombre = entry_nombre.get()
+    temperatura_str = entry_temperatura.get()
+    if not nombre or not temperatura_str:
+        messagebox.showinfo("Atención","Ingresa nombre y temperatura")
+        # text_area.insert(tk.END, "Ingresa nombre y temperatura\n")
+        return
+    try:
+        celsius = int(temperatura_str)
+    except ValueError:
+        messagebox.showinfo("Atención","Temperatura debe ser un número")
+        # text_area.insert(tk.END, "Temperatura debe ser un número\n")
+        return
+    recetario.agregar_receta(nombre, celsius)
+    text_area.delete("1.0", "end")         
+    df_recetas = recetario.mostrar_recetas()  
+    text_area.insert(tk.END, df_recetas.to_string(index=False))  
+
+    
+
+def ver_receta_por_numero():
+    try:
+        id_receta = int(entry_numero.get().strip())
+        receta = recetario.ver_receta_especifico(id_receta)
+        if not receta.empty:
+            text_area.delete("1.0", "end") 
+            text_area.insert(tk.END, f" Receta {id_receta}")
+        else:
+            messagebox.showinfo("Atención","No existe esta receta")
+            #  text_area.insert(tk.END, "No existe esa receta\n")
+        #  text_area.insert(tk.END, "Ingresa un número de ID válido\n")
+    except ValueError:
+         messagebox.showinfo("Atención","Ingresa un número de ID válido")
+
+def eliminar_receta():
+    try:
+        id_objetivo = int(entry_numero.get())
+        receta_eliminada = recetario.eliminar_receta_por_id(id_objetivo)
+        
+        if receta_eliminada:
+            text_area.delete("1.0", "end") 
+            messagebox.showinfo("Atención","Receta Eliminada")
+            df_recetas = recetario.mostrar_recetas()  
+            text_area.insert(tk.END, df_recetas.to_string(index=False)) 
+
+            # text_area.insert(tk.END, f"Receta eliminada:\n")
+        else:
+            messagebox.showinfo("Atención",f"No se encontró una receta con el ID {id_objetivo}")
+            # text_area.insert(tk.END, f"No se encontró una receta con el ID {id_objetivo}\n")
+    except ValueError:
+        messagebox.showinfo("Atención","Ingresa un número de ID válido")
+        # text_area.insert(tk.END, "Ingresa un número de ID válido\n")
+
+def actualizar_archivo():
+    df_recetas=recetario.actualizar_archivo
+    text_area.insert(tk.END, df_recetas.to_string(index=False)) 
+
+# def guardar_archivo():
+    
+
+
+
+#recetas = leer_recetas()
+#Entradas
+# Fuente en negrita para los headers
+negrita = font.Font(weight="bold")
+
+# Fila 0: Nombre y Temperatura
+tk.Label(base, text="Nombre de la receta:").grid(row=0, column=0, padx=5, pady=5)
+entry_nombre = tk.Entry(base)
+entry_nombre.grid(row=0, column=1, padx=5)
+
+tk.Label(base, text="Temperatura (°C):").grid(row=0, column=2, padx=5, pady=5)
+entry_temperatura = tk.Entry(base)
+entry_temperatura.grid(row=0, column=3, padx=5)
+
+# Botones: Registrar y Guardar
+tk.Button(base, text="Registrar Receta", bg="#90EE90", command=agregar_receta).grid(row=0, column=4, padx=5, pady=5)
+tk.Button(base, text="Guardar/Actualizar Archivo", bg="#ADD8E6", command=actualizar_archivo).grid(row=0, column=5, padx=5, pady=5)
+
+# Fila 1: Número de receta + Eliminar y Ver receta
+tk.Label(base, text="Número de receta:").grid(row=1, column=0, padx=5, pady=5)
+entry_numero = tk.Entry(base)
+entry_numero.grid(row=1, column=1, padx=5)
+
+tk.Button(base, text="Eliminar Receta", bg="#FF7F7F", command=eliminar_receta).grid(row=1, column=2, padx=5, pady=5)
+tk.Button(base, text="Ver Receta por Número", bg="#FFD700", command=ver_receta_por_numero).grid(row=1, column=3, padx=5, pady=5)
+
+# Área de texto para mostrar recetas
+text_area = tk.Text(base, width=80, height=20)
+text_area.grid(row=2, column=0, columnspan=6, padx=10, pady=10)
+
+text_area.grid_propagate(False)
+
+# Mostrar recetas automáticamente al cargar
+df_recetas = recetario.mostrar_recetas()
+if not df_recetas.empty :
+    texto_tabla = df_recetas.to_string(index=False)
+    columnas_recetas = df_recetas.columns
+    encabezado = "  ".join([str(col).upper() for col in columnas_recetas])
+    text_area.insert(tk.END, f"{encabezado}\n")
+    text_area.insert(tk.END, texto_tabla)
+else:
+    text_area.insert(tk.END, "No hay recetas registradas.")
+
+base.mainloop()
--- logica.py
+++ logica.py
@@ -7,94 +7,86 @@ import pandas as pd
 class Recetario:
     def __init__(self, archivo='recetas.csv'):
         self.archivo = archivo
-        self.campos = ['nombre', 'tiempo_coccion_min', 'temperatura_farenheit']
-        # self._asegurar_archivo()
-
+        self.campos = ['id','nombre', 'tiempo_coccion_min', 'temperatura_farenheit']
+        self.id=1
+      
     def _convertir_a_fahrenheit(self, celsius):
+        """Convierte de Cellsius a Farneheit"""
         return (celsius * 1.8) + 32
     
-   
-    def agregar_receta(self, nombre,celsius):
-        """Agrega una receta con tiempo random y temperatura  a Fahrenheit."""
+    def agregar_receta(self, nombre, celsius):
+        """Agrega una receta con tiempo random y temperatura en Fahrenheit."""
         tiempo_coccion = random.randint(15, 90)
         temperatura_farenheit = self._convertir_a_fahrenheit(celsius)
+        
         receta = {
+            'id': self.id,
             'nombre': nombre,
             'tiempo_coccion_min': tiempo_coccion,
             'temperatura_farenheit': round(temperatura_farenheit, 2)
         }
 
+        archivo_existe = os.path.isfile(self.archivo)
+        escribir_header = not archivo_existe or os.path.getsize(self.archivo) == 0
+
         with open(self.archivo, 'a', newline='') as f:
             escritor = csv.DictWriter(f, fieldnames=self.campos)
-            if not self.archivo:
+            if escribir_header:
                 escritor.writeheader()
             escritor.writerow(receta)
-        return receta   
+        self.id += 1
+        print(receta)
+        return receta
     
-    def mostrar_receta(self):
+
+    def mostrar_recetas(self):
         """Mostrar recetas del archivo"""
         if os.path.isfile(self.archivo):
             df = pd.read_csv(self.archivo)
-            print(df)
+            print("Desde mostrar",df)
             return df
         else:
             print("No hay registros disponibles.")
             return pd.DataFrame(columns=self.campos)
         
-        
     def ver_receta_especifico(self, id):
-        """Devuelve una receta por su número (índice)."""
-        recetas = self.mostrar_receta()
-        if 0 <= id < len(recetas):
-            return recetas[id]
+        """Devuelve una receta por su número (id)."""
+        recetas = self.mostrar_recetas()
+        resultado = recetas[recetas['id'] == id]
+        print(resultado,"Es errado")
+        if not resultado.empty:
+            receta = resultado.iloc[0]  
+            print("La receta en específico:", receta)
+            return receta
         else:
-            print("Id fuera del rango")
+            print("ID no existe.")
+            return None
+        
+    def eliminar_receta_por_id(self, id_eliminar):
+        recetas = self.mostrar_recetas()
+        if id_eliminar in recetas['id'].values:
+            recetas = recetas[recetas['id'] != id_eliminar]  
+            recetas.to_csv(self.archivo, index=False) 
+            print(f"Receta con id {id_eliminar} eliminada.")
+            return True
+        else:
+            print("Id no existe.")
+            return False
 
-    def eliminar_receta_especifico(self, numero):
-            """Elimina una receta por su número (índice)."""
-            recetas = self.mostrar_receta()
-            if 0 <= numero < len(recetas):
-                receta_eliminada = recetas.pop(numero)
-                with open(self.archivo, 'a', newline='') as f:
-                    escritor = csv.DictWriter(f, fieldnames=self.campos)
-                    if not self.archivo:
-                        escritor.writeheader()
-                    escritor.writerow(recetas)
-                return receta_eliminada   
-            else:
-                print("No hay registros disponibles en esta receta")
-                return None
-
-
-   
-    # def _asegurar_archivo(self):
-    #     """Crea el archivo si no existe."""
-    #     if not os.path.exists(self.archivo):
-    #         with open(self.archivo, 'w', newline='', encoding='utf-8') as f:
-    #             writer = csv.DictWriter(f, fieldnames=self.campos)
-    #             writer.writeheader()
-    #         # with open(archivo_csv, 'a', newline='') as f:
-    #         #     escritor = csv.DictWriter(f, fieldnames=fila.keys())
-    #         #     if not archivo_existe:
-    #         #         escritor.writeheader()
-    #         #     escritor.writerow(fila)
-
-
-    def actualizar_archivo(self, recetas):
+    def actualizar_archivo(self):
         """Sobrescribe el archivo con una nueva lista de recetas."""
-        with open(self.archivo, 'w', newline='') as f:
-            writer = csv.DictWriter(f, fieldnames=self.campos)
-            writer.writeheader()
-            writer.writerows(recetas)
+        archivo_existe = os.path.isfile(self.archivo)
+        escribir_header = not archivo_existe or os.path.getsize(self.archivo) == 0
+        recetas = self.mostrar_recetas()
 
-    
+        with open(self.archivo, 'a', newline='', encoding='utf-8') as f:
+            escritor = csv.DictWriter(f, fieldnames=self.campos)
+            
+            if escribir_header:
+                escritor.writeheader()
+            
+            for _, fila in recetas.iterrows():
+                escritor.writerow(fila.to_dict())
+        return recetas
 
-
-
-recetario = Recetario()
-
-
-
-recetario.agregar_receta("Receta 1", 30)
-recetario.mostrar_receta()
-
+ 
